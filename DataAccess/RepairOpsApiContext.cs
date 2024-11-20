9738e74259ae8419692cb405eda4a1460e23fead
Jakoc <damernesjacob@gmail.com>
2024-11-20 12:10:41 +0100

    test

--- DataAccess/RepairOpsApiContext.cs
+++ DataAccess/RepairOpsApiContext.cs
@@ -6,7 +6,6 @@ namespace RepairOpsApi.DataAccess;
 public class RepairOpsApiContext : DbContext
 {
     public DbSet<User> Users {get; set;}
-    
     public DbSet<Case> Cases {get; set;}
     public RepairOpsApiContext(DbContextOptions<RepairOpsApiContext> options) : base(options)
     {
