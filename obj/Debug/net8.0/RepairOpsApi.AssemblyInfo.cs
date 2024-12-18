6760e3126dd5feb9f6cbdcec35d6bfb49d8403e9
Jakoc <damernesjacob@gmail.com>
2024-12-18 12:03:16 +0100

    hallo

--- obj/Debug/net8.0/RepairOpsApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RepairOpsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepairOpsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ba1474f0a04649567f98f05a442f8b92b870d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bfdcd3ea0f7ab1a5faf3101fd55fe8d3c7409f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepairOpsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepairOpsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
